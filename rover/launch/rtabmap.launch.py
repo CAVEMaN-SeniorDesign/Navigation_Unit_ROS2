13fc134886d073baec54ee310a3636f52746ddec
DZZLab <david.zhang@pitt.edu>
2025-04-15 11:40:09 +0000

    Increased minimum obstacle height parameters in nav2_params helped quite a bit

--- rover/launch/rtabmap.launch.py
+++ rover/launch/rtabmap.launch.py
@@ -53,25 +53,26 @@ def generate_launch_description():
         realsense_launch,
         
         Node(
-            package='rtabmap_odom', executable='rgbd_odometry', output='screen',
+            package='rtabmap_odom', executable='rgbd_odometry', output='screen', respawn=True,
             parameters=parameters,
             remappings=remappings),
 
         Node(
-            package='rtabmap_slam', executable='rtabmap', output='screen',
+            package='rtabmap_slam', executable='rtabmap', output='screen', respawn=True,
             parameters=parameters,
             remappings=remappings,
             arguments=['-d']),
 
-        Node(
-            package='rtabmap_viz', executable='rtabmap_viz', output='screen',
-            parameters=parameters,
-            remappings=remappings),
+        # Disable GUI in attempt to get more performance.
+        # Node(
+        #     package='rtabmap_viz', executable='rtabmap_viz', output='screen',
+        #     parameters=parameters,
+        #     remappings=remappings),
         
         # Because of this issue: https://github.com/IntelRealSense/realsense-ros/issues/2564
         # Generate point cloud from not aligned depth
         Node(
-            package='rtabmap_util', executable='point_cloud_xyz', output='screen',
+            package='rtabmap_util', executable='point_cloud_xyz', output='screen', respawn=True,
             parameters=[{'approx_sync':False}],
             remappings=[('depth/image',       '/camera/camera/depth/image_rect_raw'),
                         ('depth/camera_info', '/camera/camera/depth/camera_info'),
@@ -79,7 +80,7 @@ def generate_launch_description():
         
         # Generate aligned depth to color camera from the point cloud above       
         Node(
-            package='rtabmap_util', executable='pointcloud_to_depthimage', output='screen',
+            package='rtabmap_util', executable='pointcloud_to_depthimage', output='screen', respawn=True,
             parameters=[{ 'decimation':2,
                           'fixed_frame_id':'camera_link',
                           'fill_holes_size':1}],
