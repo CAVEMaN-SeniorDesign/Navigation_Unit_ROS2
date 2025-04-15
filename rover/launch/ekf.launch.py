dd36b27e4e2b516d323143db161a2b3bac40e511
DZZLab <david.zhang@pitt.edu>
2025-04-15 13:58:48 +0000

    Mods to ekf

--- rover/launch/ekf.launch.py
+++ rover/launch/ekf.launch.py
@@ -9,14 +9,17 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
     odom_ekf_params = os.path.join(get_package_share_directory("rover"), 'config', 'odom_ekf.yaml')
-    imu_only_ekf_params = os.path.join(get_package_share_directory("rover"), 'config', 'only_imu_ekf.yaml')
+    VO_only_params = os.path.join(get_package_share_directory("rover"), 'config', 'only_VO.yaml')
+
+    VIO_params = os.path.join(get_package_share_directory("rover"), 'config', 'VIO.yaml')
+
 
     EKF_node = Node(
         package='robot_localization',
         executable='ekf_node',
         name='ekf_filter_node',
         output='screen',
-        parameters=[imu_only_ekf_params],
+        parameters=[VO_only_params],
     )
     
     return LaunchDescription([
