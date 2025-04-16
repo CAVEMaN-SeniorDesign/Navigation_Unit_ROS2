433939dd3553519b02dd9b1f045b4643ae516a1a
DZZLab <david.zhang@pitt.edu>
2025-04-16 14:23:36 +0000

    publishers on agx side

--- rover/src/udp_bridge_agx.py
+++ rover/src/udp_bridge_agx.py
@@ -60,6 +60,50 @@ class UdpGatewayAGX(Node):
             except Exception as e:
                 self.get_logger().warn(f"Error parsing messages from Jetson Nano: {e}")
 
+    def publish_imu(self, msg):
+        imu = Imu()
+        imu.header = Header()
+        imu.header.stamp = self.get_clock().now().to_msg()
+        imu.header.frame_id = 'imu_link'
+
+        ori = msg.get("orientation", {})
+        imu.orientation.x = ori.get("x", 0.0)
+        imu.orientation.y = ori.get("y", 0.0)
+        imu.orientation.z = ori.get("z", 0.0)
+        imu.orientation.w = ori.get("w", 1.0)
+
+        ang = msg.get("angular_velocity", {})
+        imu.angular_velocity.x = ang.get("x", 0.0)
+        imu.angular_velocity.y = ang.get("y", 0.0)
+        imu.angular_velocity.z = ang.get("z", 0.0)
+
+        lin = msg.get("linear_acceleration", {})
+        imu.linear_acceleration.x = lin.get("x", 0.0)
+        imu.linear_acceleration.y = lin.get("y", 0.0)
+        imu.linear_acceleration.z = lin.get("z", 0.0)
+
+        self.imu_pub.publish(imu)
+
+    def publish_encoders(self, msg):
+        enc = Encoders()
+        enc.total_pulses_encoder_wheel_0 = msg["wheel_0"]["pulses"]
+        enc.rate_rads_per_sec_encoder_wheel_0 = msg["wheel_0"]["rate"]
+        enc.total_pulses_encoder_wheel_1 = msg["wheel_1"]["pulses"]
+        enc.rate_rads_per_sec_encoder_wheel_1 = msg["wheel_1"]["rate"]
+        enc.total_pulses_encoder_wheel_2 = msg["wheel_2"]["pulses"]
+        enc.rate_rads_per_sec_encoder_wheel_2 = msg["wheel_2"]["rate"]
+        enc.total_pulses_encoder_wheel_3 = msg["wheel_3"]["pulses"]
+        enc.rate_rads_per_sec_encoder_wheel_3 = msg["wheel_3"]["rate"]
+        self.encoder_pub.publish(enc)
+        
+    def publish_airquality(self, msg):
+        aq = Airquality()
+        aq.dust_ug_per_m3 = msg.get("dust", 0)
+        aq.gas_ppm = msg.get("gas", 0)
+        aq.temperature_celsius = msg.get("temp", 0.0)
+        self.airquality_pub.publish(aq)
+
+
 def main():
     rclpy.init()
     node = UdpGatewayAGX()
